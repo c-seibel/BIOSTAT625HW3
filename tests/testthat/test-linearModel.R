18458e575b9c2fa4c16ddde0d9a736944af62eb9
Caitlin Seibel <cseibel@umich.edu>
2023-11-18 17:40:34 -0500

    testthat

---
+++ tests/testthat/test-linearModel.R
@@ -0,0 +1,11 @@
+test_that("linearModel works", {
+  expect_equal(as.numeric(linearModel(formula = Sepal.Length ~ Petal.Length, data = iris, print.summary = F)$coefficients),
+               as.numeric(lm(Sepal.Length ~ Petal.Length, data = iris)$coefficients))
+
+  expect_equal(as.numeric(linearModel(formula = Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species, data = iris, print.summary = F)$coefficients),
+               as.numeric(lm(Sepal.Length ~ Sepal.Width + Petal.Length + Petal.Width + Species, data = iris)$coefficients))
+
+  expect_equal(as.numeric(linearModel(formula = Sepal.Length ~ Petal.Length, data = iris, print.summary = F)$residuals),
+               as.numeric(lm(Sepal.Length ~ Petal.Length, data = iris)$residuals))
+
+})
